f0fd97ab0e644b4be90139c706298f3c80963527
Shahriar ovi <ovishahriar736@gmail.com>
2021-09-05 22:19:03 +0600

    creating java project named MyFancyJavaProject

---
+++ Assignment1.java
@@ -0,0 +1,8 @@
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello abdullah al nouman");
+        System.out.println("hello shahriar ovi");
+    }
+}
+
