196b74065d411ad315691268533c1ff7a2f21a80
Shahriar ovi <ovishahriar736@gmail.com>
2021-09-09 22:10:54 +0600

    make some slight changes in code

--- Assignment1.java
+++ Assignment1.java
@@ -1,8 +1,14 @@
 public class Assignment1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception
+    {
         System.out.println("hello abdullah al nouman");
         System.out.println("hello shahriar ovi");
+        String dbUser="admin";
+         String dbPassword="admin";
+         String dbName="tesDb";
+         DbConnection connection=DbConnection.getDbConnection(dbUser, dbPassword, dbName);
+
     }
 }
 
