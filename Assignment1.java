de24b8d1a430b9b70c78a831caee2cc98dae5113
Abdullah-Al-Nouman <alnouman172012@gmail.com>
2021-09-09 21:59:32 +0600

    singleton pattern added

--- Assignment1.java
+++ Assignment1.java
@@ -1,3 +1,43 @@
+
+class DbConnection{
+
+    private static DbConnection instance=null;
+
+    private DbConnection(String dbUser,String dbPassword,String dbName ) { 
+    	SQLConnection connection = new SQLConnection(dbUser, dbPassword, dbName);
+   }
+
+
+	public static DbConnection getDbConnection(String dbUser,String dbPassword,String dbName) {
+      if (instance== null ) 
+		instance = new DbConnection(dbUser,dbPassword,dbName) ;                           
+
+      return instance;
+    }
+}
+
+class SQLConnection {
+	
+	String dbUser;
+	String dbPassword;
+	String dbName;
+	
+	public SQLConnection(String dbUser, String dbPassword, String dbName) {
+		super();
+		this.dbUser = dbUser;
+		this.dbPassword = dbPassword;
+		this.dbName = dbName;
+	}
+	
+
+}
+
+
+
+
+
+
+
 public class Assignment1 {
 
     public static void main(String[] args) {
